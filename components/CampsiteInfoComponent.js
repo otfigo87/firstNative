3d3859b7fcce3a276275c1f64840251ab3c7b424
Otmane Aatik <aaotmane@gmail.com>
2021-12-14 17:02:42 -0500

    navigation part 1

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
 
 const RenderCampsite = ({campsite}) => {
@@ -18,8 +19,21 @@ const RenderCampsite = ({campsite}) => {
     return <View/>
 }
 
-const CampsiteInfo = (props) => {
-   return <RenderCampsite campsite={props.campsite}/>
+class CampsiteInfo extends Component  {
+    constructor(props){
+        super(props);
+        this.state = { campsites: CAMPSITES }
+    }
+
+    static navigationOptions = {
+        title: 'Campsite Information'
+    }
+
+    render(){
+        const campsiteId = this.props.navigation.getParam('campsiteId');
+        const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
+        return <RenderCampsite campsite={campsite}/>
+}
 }
 
 export default CampsiteInfo;
