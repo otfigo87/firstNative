5397db198d54c483a60a31699ded1cae328f94d0
Otmane Aatik <aaotmane@gmail.com>
2021-12-23 15:11:21 -0500

    forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
+import Reservation from './ReservationComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import Constants from 'expo-constants';
 import { createStackNavigator} from 'react-navigation-stack';
@@ -100,6 +101,25 @@ const ContactNavigator = createStackNavigator(
                             />
         })
     }
+);
+
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: {screen: Reservation}
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ( {
+            headerStyle: { backgroundColor: '#5637dd'},
+            headerTintColor: '#fff',
+            headerTitleStyle: { color: '#fff'},
+            headerLeft: <Icon 
+                              name="tree"
+                              type="font-awesome"
+                              iconStyle={styles.stackIcon}
+                              onPress={() => navigation.toggleDrawer()}
+                            />
+        })
+    }
 )
 
 //////////////Drawer
@@ -146,6 +166,17 @@ const MainNavigator = createDrawerNavigator(
                     
                     />)
                }},
+        Reservation: {screen: ReservationNavigator,
+                    navigationOptions: {
+                      drawerLabel:'Reserve Campsite',  
+                      drawerIcon: ({tintColor}) => (<Icon
+                                        name="tree"
+                                        type="font-awesome"
+                                        size={24}
+                                        color={tintColor}
+                    
+                    />)
+               }},       
         About: {screen: AboutNavigator,
                 navigationOptions: {
                     drawerLabel: "About Us",
